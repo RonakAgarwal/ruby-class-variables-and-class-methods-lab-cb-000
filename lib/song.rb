7fcf748adb166aea3c31015558b18ab9adef8ce3
RonakAgarwal <agarwalronak02@gmail.com>
2018-08-02 13:50:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,13 @@
 def Song
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
   attr_accessor :name, :artist, :genre
 
   @@count = 0
